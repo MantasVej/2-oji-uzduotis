60d4a6a3f75abb33b29a6a58030d10b2bf30c3c5
Mantas Vėjelis <92882227+MantasVej@users.noreply.github.com>
2022-02-18 19:31:25 +0200

    Update Source.cpp

--- Source.cpp
+++ Source.cpp
@@ -2,7 +2,12 @@
 #include <string>
 #include <iomanip>
 
-using namespace std;
+using std::cout;
+using std::cin;
+using std::string;
+using std::left;
+using std::setw;
+using std::endl;
 
 struct Mokinys
 {
@@ -12,31 +17,85 @@ struct Mokinys
     int egzaminas = 0;
     double galutinis = 0;
 };
-void Ivestis(Mokinys& temp) {
-    int n = 3;
-    double sum = 0;
+//Skaiciuoja galutini pazymi pagal vidurki
+void Vidurkis(Mokinys& temp, int n) {
+    int sum = 0; //pazymiu suma
+    for (int i = 0; i < n; i++) sum += temp.P[i];
+    temp.galutinis = 0.4 * (sum / n) + 0.6 * temp.egzaminas;
+};
+//Skaiciuoja galutini pazymi pagal mediana
+void Mediana(Mokinys& temp, int n) {
+    double mediana = 0; //pazymiu mediana
+    for (int i = 0; i < n; i++)
+        for (int j = i+1; j <= n; j++) if (temp.P[i] > temp.P[j]) std::swap(temp.P[i], temp.P[j]);
+    if ((n % 2) == 0) mediana = (temp.P[n / 2] + temp.P[(n / 2) - 1]) / 2.0;
+    else mediana = temp.P[n/2];
+    temp.galutinis = 0.4 * mediana + 0.6 * temp.egzaminas;
+};
+//Tikrina ar teisingai ivedamas pazymys
+void Pazymys(Mokinys& temp, int i) {
+    cout << "Iveskite " << i + 1 << "-a pazymi: ";
+    if (!(cin >> temp.P[i])) {
+        cout << "Neteisinga ivestis. Bandykite dar karta " << endl;
+        cin.clear();
+        cin.ignore(10000,'\n');
+        Pazymys(temp, i);
+    }
+}
+//Skaito duomenis apie studenta
+void Ivestis(Mokinys& temp, int n) {
     cout << "Iveskite studento varda: "; cin >> temp.vardas;
     cout << "Iveskite studento pavarde: "; cin >> temp.pavarde;
     for (int i = 0; i < n; i++) {
-        cout << "Iveskite " << i + 1 << "-a pazymi: "; cin >> temp.P[i];
-        sum += temp.P[i];
+        Pazymys(temp, i);
     }
     cout << "Iveskite egzamino rezultata: "; cin >> temp.egzaminas;
-    temp.galutinis = 0.4 * (sum / n) + 0.6 * temp.egzaminas;
 }
-void Isvestis(Mokinys& temp) {
-    cout << left << setw(15) << temp.vardas << left << setw(15) << temp.pavarde << fixed << setprecision(2) << left << setw(15) << temp.galutinis << endl;
+//Isveda rezultatus
+void Isvestis(Mokinys& temp, int n, char rez) {
+    if (rez == 'v') Vidurkis(temp, n);
+    else Mediana(temp, n);
+    cout << left << setw(15) << temp.vardas << left << setw(15) << temp.pavarde << std::fixed << std::setprecision(2) << left << setw(15) << temp.galutinis << endl;
+}
+//Perkopijuoja studentu duomenis i kita masyva dinamineje atmintyje
+void Kopijuoti(Mokinys*& temp, int dydis) {
+    Mokinys* kopija = new Mokinys[dydis];
+    for (int i = 0; i < dydis - 1; i++)
+        kopija[i] = temp[i];
+    delete [] temp;
+    temp = kopija;
 }
 int main()
 {
-    Mokinys* mas = new Mokinys[3];
-    for (int i = 0; i < 3; i++) {
-        Ivestis(mas[i]);
+    int dydis = 1; //pradinis studentu skaicius
+    Mokinys* mas = new Mokinys[dydis];
+    int n = 3; // namu darbu skaicius
+    bool check; //kintamasis skirtas tikrinti duomenu ivesciai
+    char stud;
+    char rez;
+    for (int i = 0; i < dydis; i++) {
+        Ivestis(mas[i], n);
+        do {
+            cout << "Ar norite ivesti kita studenta (t/n)? "; cin >> stud;
+            check = 1;
+            if (stud == 't') {
+                dydis++;
+                Kopijuoti(mas, dydis);
+            }
+            else if (stud == 'n');
+            else { cout << "Neteisinga ivestis. Bandykite dar karta" << endl; check = 0; }
+        } while (!check);
     }
-    cout << "Vardas         Pavarde        Galutinis(vid.)" << endl;
+    do {
+        cout << "Galutini rezultata pateikti pagal vidurki/mediana (v/m)? "; cin >> rez;
+        check = 1;
+        if (rez == 'v') cout << "Vardas         Pavarde        Galutinis(vid.)" << endl;
+        else if (rez == 'm') cout << "Vardas         Pavarde        Galutinis(med.)" << endl;
+        else {cout << "Neteisinga ivestis. Bandykite dar karta" << endl; check = 0;}
+    } while (!check);
     cout << "---------------------------------------------" << endl;
-    for (int i = 0; i < 3; i++) {
-        Isvestis(mas[i]);
+    for (int i = 0; i < dydis; i++) {
+        Isvestis(mas[i], n, rez);
     }
     cout << "---------------------------------------------" << endl;
     delete[] mas;
