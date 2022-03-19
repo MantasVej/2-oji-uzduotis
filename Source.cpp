deade84978fc5cf1460620dfe17fb5b3969296ab
Mantas Vėjelis <92882227+MantasVej@users.noreply.github.com>
2022-03-19 21:33:30 +0200

    Update Source.cpp

--- Source.cpp
+++ Source.cpp
@@ -3,8 +3,11 @@
 
 int main()
 {
+    auto start = std::chrono::high_resolution_clock::now(); auto st=start;
     vector<Mokinys> mas;
     mas.push_back(Mokinys());
     Studentai(mas);
+    std::chrono::duration<double> diff = std::chrono::high_resolution_clock::now()-start;
+    cout << "Visos programos veikimas uztruko: "<< diff.count() << " s\n";
     return 0;
 }
