15dc896d170804a174f86660749e9da5bb1de4cf
Mantas Vėjelis <92882227+MantasVej@users.noreply.github.com>
2022-04-01 22:50:40 +0300

    Deque

---
+++ Deque/Header.h
@@ -0,0 +1,34 @@
+#pragma once
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <iomanip>
+#include <ctime>
+#include <deque>
+#include <chrono>
+#include <random>
+#include <sstream>
+#include <exception>
+#include "Struktura.h"
+
+using std::cout;
+using std::cin;
+using std::left;
+using std::setw;
+using std::endl;
+using std::mt19937;
+
+
+using hrClock = std::chrono::high_resolution_clock;
+typedef std::uniform_int_distribution<int>  int_distribution;
+
+void Studentai(deque<Mokinys>&);
+void Vidurkis(Mokinys&);
+void Mediana(Mokinys&);
+int Generavimas();
+void Ivestis(Mokinys&);
+void Isvestis(Mokinys&, char);
+int Tarpai(string eil);
+void Skaitymas(deque<Mokinys>&, int&, string);
+void FailoGeneravimas(int, int, string&);
+void Rusiavimas(deque<Mokinys>&, int, char);
