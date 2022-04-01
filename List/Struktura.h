ccc2ea73c69383026410cad7ab4f2b548c3a9910
Mantas Vėjelis <92882227+MantasVej@users.noreply.github.com>
2022-04-01 22:43:20 +0300

    List

---
+++ List/Struktura.h
@@ -0,0 +1,14 @@
+#pragma once
+
+using std::string;
+using std::list;
+
+struct Mokinys
+{
+    string vardas = "";
+    string pavarde = "";
+    int n = 0;
+    list <int> v;
+    int egzaminas = 0;
+    double galutinis = 0;
+};
