772cae71870e2df38751ce6e7dd24745312264e5
Mantas Vėjelis <92882227+MantasVej@users.noreply.github.com>
2022-04-15 20:18:59 +0300

    Optimised Vector

---
+++ Optimised/Vector/Struktura.h
@@ -0,0 +1,14 @@
+#pragma once
+
+using std::string;
+using std::vector;
+
+struct Mokinys
+{
+    string vardas = "";
+    string pavarde = "";
+    int n = 0;
+    vector <int> v;
+    int egzaminas = 0;
+    double galutinis = 0;
+};
